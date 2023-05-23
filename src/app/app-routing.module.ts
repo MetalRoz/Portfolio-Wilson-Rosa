5f40e060e58436d4a89911b1c59524ef2341f686
Wilson <wilsonmiguel2003@gmail.com>
2023-05-23 16:38:27 -0400

    agregando proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { SuccessComponent } from './components/success/success.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { VentasComponent } from './components/ventas/ventas.component';
 
 const routes: Routes = [
   {path: "", component: HomePageComponent},
-  {path:"success", component: SuccessComponent}
+  {path:"success", component: SuccessComponent},
+  {path: "ventas", component: VentasComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SuccessComponent } from './components/success/success.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { VentasComponent } from './components/ventas/ventas.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     ContactComponent,
     HeaderComponent,
     SuccessComponent,
-    HomePageComponent
+    HomePageComponent,
+    VentasComponent
   ],
   imports: [
     BrowserModule,
